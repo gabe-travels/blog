cdadd05bd97df8d2c8d52af5cbb18b483a794e02
Gabe <>
2023-05-02 18:00:23 +0700

    Change article URLs to be independent of post title: YYYY-MM-DD

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,4 +4,6 @@ SITEURL = 'https://gabe-travels.github.io'
 PATH = 'content'
 TIMEZONE = 'America/New_York'
 
+ARTICLE_URL = ARTICLE_SAVE_AS = '{date:%Y}-{date:%m}-{date:%d}.html'
+
 GOOGLE_ANALYTICS = 'G-QT3EM5BQV4'
